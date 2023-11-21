d84c11ce69ce8bff7399d775f82a41fc91d186dd
brendanv <brendanv@users.noreply.github.com>
2023-11-21 04:37:38 +0000

    Basic Link model

--- lynx/models.py
+++ lynx/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Link(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    url = models.URLField(max_length=1000)
+    hostname = models.CharField(max_length=200, blank=True)
+    article_date = models.DateField(null=True) # Published date of the article
+    author = models.CharField(max_length=200, blank=True)
+    title = models.CharField(max_length=200, blank=True)
+    excerpt = models.TextField(blank=True)
+
+    # Parsed/generated content
+    summary = models.TextField(blank=True) # AI summary if requested
+    markdown_content = models.TextField(blank=True)
+    json_parse_result = models.JSONField(null=True, blank=False)
+    header_image_url = models.URLField(blank=True, default="")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created_at']
