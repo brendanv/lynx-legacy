f59bd60b070f55b408f379373be82b12eff3b66a
brendanv <brendanv@users.noreply.github.com>
2023-11-22 03:34:15 +0000

    Basic view frameworks for links list and details and readable pages

--- lynx/urls.py
+++ lynx/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'lynx'
 urlpatterns = [
     path("", views.index, name="index"),
-    path("link/<int:link_id>", views.readable, name="readable"),
+    path("links/all", views.FeedView.as_view(), name="links_feed"),
+    path("link/<int:pk>/details/", views.DetailsView.as_view(), name="link_details"),
+    path("link/<int:pk>/view", views.ReadableView.as_view(), name="link_viewer"),
     path("test_parse", views.test_parse, name="test_parse")
 ]
