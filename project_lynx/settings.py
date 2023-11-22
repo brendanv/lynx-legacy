0dfa7c3186aac75b5f694a03341b084e2667ef0f
brendanv <brendanv@users.noreply.github.com>
2023-11-22 03:06:38 +0000

    Add Link to admin, and add Replit host to allowed hosts

--- project_lynx/settings.py
+++ project_lynx/settings.py
@@ -120,3 +120,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://lynx--replit868.repl.co']
