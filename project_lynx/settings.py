1181887402283cdf11c52cc9da6b8efbb6643ec4
brendanv <brendanv@users.noreply.github.com>
2023-11-22 03:37:19 +0000

    Move secret key to environment variable

--- project_lynx/settings.py
+++ project_lynx/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-huspmh2sj4t9w%g)fog-6@+msb90$=dc&ik!z)=bl604axms##'
+SECRET_KEY = os.getenv('SECRET_KEY')
+if SECRET_KEY is None:
+  print("Please setup a SECRET_KEY environment variable")
+  exit(1)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
