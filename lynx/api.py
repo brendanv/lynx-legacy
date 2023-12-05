13ea68fd662c5712a2002d683b9eac4b7c810f40
brendanv <brendanv@users.noreply.github.com>
2023-12-05 03:16:47 +0000

    Add basic Create Link API endpoint

--- lynx/api.py
+++ lynx/api.py
@@ -1,7 +1,8 @@
 from django.http import HttpRequest
-from ninja import NinjaAPI
+from ninja import NinjaAPI, Schema
 from ninja.security import HttpBearer, APIKeyHeader
-from lynx.models import UserSetting
+from lynx.models import UserSetting, Link
+from lynx import url_parser
 from typing import Any, Optional
 
 api = NinjaAPI()
@@ -29,7 +30,21 @@ class LynxApiKeyBearer(LynxKeyAuthenticator, HttpBearer):
 
 lynx_auth_methods = [LynxApiKeyBearer(), LynxApiKeyHeader()]
 
+class LinkCreate(Schema):
+  url: str
 
-@api.post("/test", auth=lynx_auth_methods)
-def hello(request):
-  return "Success"
+class LinkOverview(Schema):
+  id: int
+  original_url: str
+  cleaned_url: str
+  title: str = None
+  author: str = None
+  read_time_seconds: int = None
+  read_time_display: str = None
+
+@api.post("/links/add", auth=lynx_auth_methods, response=LinkOverview)
+def create_link(request, link_create: LinkCreate):
+  assert isinstance(request.auth, UserSetting)
+  url = url_parser.parse_url(link_create.url, request.auth.user)
+  url.save()
+  return url
--- lynx/tests/test_api.py
+++ lynx/tests/test_api.py
@@ -1,41 +1,103 @@
 from django.contrib.auth.models import User
 from django.test import TestCase, Client
-from lynx.models import UserSetting
+from unittest.mock import patch
+from lynx.models import UserSetting, Link
+import json
+from django.utils import timezone
+
+add_link_dict = {"url": "https://example.com?1234=5678"}
 
 
-class ApiEndpointTests(TestCase):
+class CreateLinkEndpointTest(TestCase):
 
   def setUp(self):
     self.client = Client()
     self.user = User.objects.create_user(username='testuser')
-    self.user_setting = UserSetting.objects.create(
-        user_id=self.user.pk,
-        lynx_api_key='test_api_key')
+    self.user_setting = UserSetting.objects.create(user_id=self.user.pk,
+                                                   lynx_api_key='test_api_key')
 
-  def test_test_endpoint_with_valid_key_header(self):
-    response = self.client.post('/api/test', HTTP_X_API_KEY='test_api_key')
+  def create_test_link(self, **kwargs) -> Link:
+    default_user = self.user
+    defaults = {
+        'summary': '',
+        'raw_text_content': 'Some content',
+        'article_date': timezone.now(),
+        'read_time_seconds': 12,
+        'creator': default_user,
+    }
+    defaults.update(kwargs)
+    link = Link(**defaults)
+    link.save()
+    return link
+
+  @patch('lynx.url_parser.parse_url')
+  def test_test_endpoint_with_valid_key_header(self, mock_parse_url):
+    link = self.create_test_link(
+        original_url=add_link_dict['url'],
+        cleaned_url='https://example.com',
+        title='Example Title',
+        author='Example Author',
+        read_time_seconds=12,
+        read_time_display='12 seconds',
+    )
+    mock_parse_url.return_value = link
+    response = self.client.generic('POST',
+                                   '/api/links/add',
+                                   json.dumps(add_link_dict),
+                                   HTTP_X_API_KEY='test_api_key')
     self.assertEqual(response.status_code, 200)
-    self.assertEqual(response.content.decode(), '"Success"')
-
-  def test_test_endpoint_with_invalid_key_header(self):
-    response = self.client.post('/api/test', HTTP_X_API_KEY='invalid_key')
-    self.assertNotEqual(response.status_code, 200)
-
-  def test_test_endpoint_with_no_key_header(self):
-    response = self.client.post('/api/test')
-    self.assertNotEqual(response.status_code, 200)
-
-  def test_test_endpoint_with_valid_key_bearer(self):
-    response = self.client.post('/api/test',
-                                HTTP_AUTHORIZATION='Bearer test_api_key')
+    mock_parse_url.assert_called_once_with(add_link_dict['url'], self.user)
+    response_data = json.loads(response.content.decode())
+    self.assertEqual(response_data['original_url'], add_link_dict['url'])
+    self.assertEqual(response_data['cleaned_url'], link.cleaned_url)
+    self.assertEqual(response_data['title'], link.title)
+    self.assertEqual(response_data['author'], link.author)
+    self.assertEqual(response_data['read_time_seconds'],
+                     link.read_time_seconds)
+    self.assertEqual(response_data['read_time_display'],
+                     link.read_time_display)
+    
+  @patch('lynx.url_parser.parse_url')
+  def test_test_endpoint_with_valid_key_bearer(self, mock_parse_url):
+    link = self.create_test_link(
+        original_url=add_link_dict['url'],
+        cleaned_url='https://example.com',
+        title='Example Title',
+        author='Example Author',
+        read_time_seconds=12,
+        read_time_display='12 seconds',
+    )
+    mock_parse_url.return_value = link
+    response = self.client.generic('POST',
+                                   '/api/links/add',
+                                   json.dumps(add_link_dict),
+                                   HTTP_AUTHORIZATION='Bearer test_api_key')
     self.assertEqual(response.status_code, 200)
-    self.assertEqual(response.content.decode(), '"Success"')
-
-  def test_test_endpoint_with_invalid_key_bearer(self):
-    response = self.client.post('/api/test',
-                                HTTP_AUTHORIZATION='Bearer invalid_key')
+    mock_parse_url.assert_called_once_with(add_link_dict['url'], self.user)
+    response_data = json.loads(response.content.decode())
+    self.assertEqual(response_data['original_url'], add_link_dict['url'])
+    self.assertEqual(response_data['cleaned_url'], link.cleaned_url)
+    self.assertEqual(response_data['title'], link.title)
+    self.assertEqual(response_data['author'], link.author)
+    self.assertEqual(response_data['read_time_seconds'],
+                     link.read_time_seconds)
+    self.assertEqual(response_data['read_time_display'],
+                     link.read_time_display)
+    
+  @patch('lynx.url_parser.parse_url')
+  def test_test_endpoint_with_invalid_key_bearer(self, mock_parse_url):
+    response = self.client.generic('POST',
+                                   '/api/links/add',
+                                   json.dumps(add_link_dict),
+                                   HTTP_AUTHORIZATION='Bearer invalid_key')
     self.assertNotEqual(response.status_code, 200)
-
-  def test_test_endpoint_with_no_key_bearer(self):
-    response = self.client.post('/api/test')
+    mock_parse_url.assert_not_called()
+    
+  @patch('lynx.url_parser.parse_url')
+  def test_test_endpoint_with_invalid_key_header(self, mock_parse_url):
+    response = self.client.generic('POST',
+                                   '/api/links/add',
+                                   json.dumps(add_link_dict),
+                                   HTTP_X_API_KEY='invalid_key')
     self.assertNotEqual(response.status_code, 200)
+    mock_parse_url.assert_not_called()
