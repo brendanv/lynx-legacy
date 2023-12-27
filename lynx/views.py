44bc978508c690d5360f990949ce8e922591588f
brendanv <brendanv@users.noreply.github.com>
2023-12-27 19:17:04 +0000

    Switch to function based views

--- lynx/views.py
+++ lynx/views.py
@@ -1,9 +1,6 @@
-from typing import Any
 from asgiref.sync import sync_to_async
-from django.db.models import QuerySet
-from django.http import HttpResponseRedirect, JsonResponse
-from django.urls import reverse
-from django.views import generic, View
+from django.http import JsonResponse, HttpResponseNotAllowed, HttpRequest, HttpResponse
+from django.template.response import TemplateResponse
 from django.utils import timezone
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django import forms
@@ -14,11 +11,36 @@ from lynx import feed_utils, url_parser, url_summarizer, html_cleaner
 from lynx.models import FeedItem, Link, UserSetting, UserCookie, Feed
 from lynx.errors import NoAPIKeyInSettings
 import secrets
+import functools
+from django.contrib.auth.views import redirect_to_login
+from django.shortcuts import aget_object_or_404, redirect
+
+
+def async_login_required(view_func):
+
+  @functools.wraps(view_func)
+  async def wrapper(request: HttpRequest, *args, **kwargs):
+    if not await (sync_to_async(lambda: request.user.is_authenticated)()):
+      return redirect_to_login(request.get_full_path())
+    return await view_func(request, *args, **kwargs)
+
+  return wrapper
+
+
+def lynx_post_only(view_func):
+
+  @functools.wraps(view_func)
+  async def wrapper(request, *args, **kwargs):
+    if request.method != 'POST':
+      return HttpResponseNotAllowed(['POST'])
+    return await view_func(request, *args, **kwargs)
+
+  return wrapper
 
 
 class APIKeyWidget(TextInput):
   template_name = "widgets/api_key_widget.html"
-  
+
   def __init__(self, **kwargs):
     super().__init__(**kwargs)
     self.attrs['readonly'] = ''
@@ -46,115 +68,102 @@ class AddLinkForm(forms.Form):
                        max_length=2000,
                        widget=FancyTextWidget('Article URL'))
 
-  def create_link(self, user):
+  def create_link(self, user) -> Link:
     url = url_parser.parse_url(self.cleaned_data['url'], user)
     url.save()
     return url
 
 
-class AddLinkView(LoginRequiredMixin, generic.FormView):
-  template_name = 'lynx/add_link.html'
-  form_class = AddLinkForm
+@async_login_required
+async def add_link_view(request: HttpRequest) -> HttpResponse:
+  if request.method == 'POST':
+    form = AddLinkForm(request.POST)
+    if form.is_valid():
+      link = await (sync_to_async(lambda: form.create_link(request.user))())
+      return redirect('lynx:link_viewer', pk=link.pk)
+  else:
+    form = AddLinkForm()
 
-  def form_valid(self, form):
-    link = form.create_link(self.request.user)
-    return HttpResponseRedirect(reverse("lynx:link_viewer", args=(link.id, )))
-
-
-class SummarizeLinkView(View):
-
-  async def post(self, request, pk):
-    try:
-      if not await (sync_to_async(lambda: request.user.is_authenticated)()):
-        return JsonResponse(
-            {"error": "You must be logged in to summarize a link."})
-      link = await Link.objects.aget(pk=pk, creator=request.user)
-      link = await url_summarizer.generate_and_persist_summary(link)
-
-      return HttpResponseRedirect(
-          reverse("lynx:link_details", args=(link.pk, )))
-    except Link.DoesNotExist:
-      return JsonResponse({"error": "Link does not exist."})
-    except NoAPIKeyInSettings:
-      return JsonResponse(
-          {"error": "You must have an OpenAI API key in your settings."})
+  return TemplateResponse(request, 'lynx/add_link.html', {'form': form})
 
 
-class ReadableView(LoginRequiredMixin, generic.DetailView):
-  model = Link
-  template_name = "lynx/link_viewer.html"
+@async_login_required
+@lynx_post_only
+async def summarize_link_view(request: HttpRequest, pk: int) -> HttpResponse:
+  try:
+    link = await aget_object_or_404(Link, pk=pk, creator=request.user)
+    link = await url_summarizer.generate_and_persist_summary(link)
 
-  def get_queryset(self):
-    return Link.objects.filter(creator=self.request.user)
-
-  def get_object(self):
-    obj = super().get_object()
-    obj.last_viewed_at = timezone.now()
-    obj.save()
-    return obj
-
-  def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-    data = super().get_context_data(**kwargs)
-    cleaner = html_cleaner.HTMLCleaner(data['link'].article_html)
-    cleaner.generate_headings().replace_image_links_with_images()
-    data['html_with_sections'] = cleaner.prettify()
-    data['table_of_contents'] = [h.to_dict() for h in cleaner.get_headings()]
-    return data
+    return redirect('lynx:link_details', pk=link.pk)
+  except NoAPIKeyInSettings:
+    return JsonResponse(
+        {"error": "You must have an OpenAI API key in your settings."})
 
 
-class DetailsView(LoginRequiredMixin, generic.DetailView):
-  model = Link
-  template_name = "lynx/link_details.html"
+@async_login_required
+async def readable_view(request: HttpRequest, pk: int) -> HttpResponse:
+  link = await Link.objects.aget(pk=pk, creator=request.user)
+  context_data = {'link': link}
+  cleaner = html_cleaner.HTMLCleaner(link.article_html)
+  cleaner.generate_headings().replace_image_links_with_images()
+  context_data = {
+      'link': link,
+      'html_with_sections': cleaner.prettify(),
+      'table_of_contents': [h.to_dict() for h in cleaner.get_headings()]
+  }
 
-  def get_queryset(self):
-    return Link.objects.filter(creator=self.request.user)
+  link.last_viewed_at = timezone.now()
+  await link.asave()
+  return TemplateResponse(request, "lynx/link_viewer.html", context_data)
 
 
-class LinkFeedView(LoginRequiredMixin, generic.ListView):
-  template_name = "lynx/links_feed.html"
-  context_object_name = "links_list"
-  paginate_by = 25
-  filter = None
+@async_login_required
+async def details_view(request: HttpRequest, pk: int) -> HttpResponse:
+  link = await aget_object_or_404(Link, pk=pk, creator=request.user)
+  return TemplateResponse(request, "lynx/link_details.html", {'link': link})
 
-  def get_queryset(self):
-    query_string = self.request.GET.get('q', '')
-    if query_string:
-      sql = '''
-        SELECT lynx_link.*, rank 
-        FROM lynx_link 
-        INNER JOIN (
-          SELECT rowid, rank 
-          FROM lynx_link_fts 
-          WHERE lynx_link_fts MATCH %s
-        ) 
-        ON lynx_link.id = rowid 
-        ORDER BY rank;
-      '''
-      return Link.objects.raw(sql, [query_string])
-    else:
-      queryset = Link.objects.filter(creator=self.request.user)
-      if self.filter == "read":
-        queryset = queryset.filter(last_viewed_at__isnull=False)
-      elif self.filter == "unread":
-        queryset = queryset.filter(last_viewed_at__isnull=True)
 
-      return queryset.order_by('-created_at')
+@async_login_required
+async def link_feed_view(request: HttpRequest, filter: str = "all") -> HttpResponse:
+  query_string = request.GET.get('q', '')
+  if query_string:
+    sql = '''
+      SELECT lynx_link.*, rank 
+      FROM lynx_link 
+      INNER JOIN (
+        SELECT rowid, rank 
+        FROM lynx_link_fts 
+        WHERE lynx_link_fts MATCH %s
+      ) 
+      ON lynx_link.id = rowid 
+      WHERE lynx_link.creator_id = %s
+      ORDER BY rank;
+    '''
+    queryset = Link.objects.raw(sql, [query_string, request.user.id])
+  else:
+    queryset = Link.objects.filter(creator=request.user)
+    if filter == "read":
+      queryset = queryset.filter(last_viewed_at__isnull=False)
+    elif filter == "unread":
+      queryset = queryset.filter(last_viewed_at__isnull=True)
 
-  def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-    data = super().get_context_data(**kwargs)
-    data['selected_filter'] = self.filter
-    data['query'] = self.request.GET.get('q', '')
+    queryset = queryset.order_by('-created_at')
 
-    if self.filter == "read":
-      data['title'] = "Read Links"
-    elif self.filter == "unread":
-      data['title'] = "Unread Links"
-    elif self.filter == "search":
-      data['title'] = "Search Results"
-    else:
-      data['title'] = "All Links"
+  data = {}
+  data['selected_filter'] = filter
+  data['query'] = query_string
 
-    return data
+  if filter == "read":
+    data['title'] = "Read Links"
+  elif filter == "unread":
+    data['title'] = "Unread Links"
+  elif filter == "search":
+    data['title'] = "Search Results"
+  else:
+    data['title'] = "All Links"
+
+  data['links_list'] = await (sync_to_async(list)(queryset))
+  return TemplateResponse(request, "lynx/links_feed.html", context=data)
 
 
 class UpdateSettingsForm(forms.Form):
@@ -180,21 +189,24 @@ class UpdateSettingsForm(forms.Form):
     setting.save()
 
 
-class UpdateSettingsView(LoginRequiredMixin, generic.FormView):
-  template_name = 'lynx/usersetting_form.html'
-  form_class = UpdateSettingsForm
+@async_login_required
+async def update_settings_view(request: HttpRequest) -> HttpResponse:
+  setting, _ = await UserSetting.objects.aget_or_create(user=request.user)
+  if request.method == 'POST':
+    form = UpdateSettingsForm(request.POST)
+    if form.is_valid():
+      await (sync_to_async(lambda: form.update_setting(request.user))())
+      messages.success(request, "Settings updated.")
+      return redirect('lynx:user_settings')
+  else:
+    form = UpdateSettingsForm(
+        initial={
+            'openai_api_key': setting.openai_api_key,
+            'lynx_api_key': setting.lynx_api_key,
+        })
 
-  def form_valid(self, form):
-    form.update_setting(self.request.user)
-    messages.success(self.request, "Settings updated.")
-    return HttpResponseRedirect(reverse("lynx:user_settings"))
-
-  def get_initial(self):
-    setting, _ = UserSetting.objects.get_or_create(user=self.request.user)
-    initial = super().get_initial()
-    initial['openai_api_key'] = setting.openai_api_key
-    initial['lynx_api_key'] = setting.lynx_api_key
-    return initial
+  return TemplateResponse(request, "lynx/usersetting_form.html",
+                          {'form': form})
 
 
 class UpdateCookiesView(LoginRequiredMixin, ModelFormSetView):
@@ -238,13 +250,13 @@ class UpdateCookiesView(LoginRequiredMixin, ModelFormSetView):
     return UserCookie.objects.filter(user=self.request.user)
 
 
-class FeedListView(LoginRequiredMixin, generic.ListView):
-  template_name = "lynx/feed_list.html"
-  model = Feed
-  context_object_name = "feeds_list"
-
-  def get_queryset(self) -> QuerySet[Feed]:
-    return Feed.objects.filter(user=self.request.user, is_deleted=False)
+@async_login_required
+async def feeds_list_view(request: HttpRequest) -> HttpResponse:
+  feeds = await (sync_to_async(list)(Feed.objects.filter(user=request.user,
+                                                         is_deleted=False)))
+  return TemplateResponse(request, 'lynx/feed_list.html', {
+      'feeds_list': feeds,
+  })
 
 
 class AddFeedForm(forms.Form):
@@ -263,84 +275,83 @@ class AddFeedForm(forms.Form):
     return loader.get_feed()
 
 
-class AddFeedView(LoginRequiredMixin, generic.FormView):
-  template_name = 'lynx/add_feed.html'
-  form_class = AddFeedForm
+@async_login_required
+async def add_feed_view(request: HttpRequest) -> HttpResponse:
+  if request.method == 'POST':
+    form = AddFeedForm(request.POST)
+    if form.is_valid():
+      await (sync_to_async(lambda: form.create_feed(request, request.user))())
+      return redirect('lynx:feeds')
+  else:
+    form = AddFeedForm()
 
-  def form_valid(self, form):
-    form.create_feed(self.request, self.request.user)
-    return HttpResponseRedirect(reverse("lynx:feeds", args=()))
-
-
-class FeedItemListView(LoginRequiredMixin, generic.ListView):
-  template_name = "lynx/feed_item_list.html"
-  model = FeedItem
-  context_object_name = "feed_items_list"
-
-  def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-    context = super().get_context_data(**kwargs)
-    context['feed'] = Feed.objects.get(pk=self.kwargs['feed_id'])
-    return context
-
-  def get_queryset(self) -> QuerySet[FeedItem]:
-    return FeedItem.objects.filter(feed=self.kwargs['feed_id'])
+  return TemplateResponse(request, 'lynx/add_feed.html', {'form': form})
 
 
-class AddFeedItemToLibraryView(LoginRequiredMixin, View):
-
-  def post(self, request, pk):
-    try:
-      feed_item = FeedItem.objects.get(pk=pk)
-      feed = feed_item.feed
-
-      if feed.user != request.user:
-        raise FeedItem.DoesNotExist()
-
-      url = feed_item.saved_as_link
-      if url is None:
-        url = url_parser.parse_url(feed_item.url, request.user)
-        url.created_from_feed = feed
-        url.save()
-
-        feed_item.saved_as_link = url
-        feed_item.save()
-
-      return HttpResponseRedirect(reverse("lynx:feed_items", args=(feed.pk, )))
-    except FeedItem.DoesNotExist:
-      return JsonResponse({"error": "FeedItem does not exist."})
+@async_login_required
+async def feed_items_list_view(request: HttpRequest,
+                               feed_id: int) -> HttpResponse:
+  feed = await aget_object_or_404(Feed.objects.select_related('user'),
+                                  pk=feed_id,
+                                  is_deleted=False)
+  if feed.user != request.user:
+    raise Feed.DoesNotExist()
+  feed_items = await (sync_to_async(list)(
+      FeedItem.objects.filter(feed=feed).order_by('-pub_date')))
+  return TemplateResponse(request, 'lynx/feed_item_list.html', {
+      'feed_items_list': feed_items,
+      'feed': feed
+  })
 
 
-class RefreshFeedFromRemoteView(LoginRequiredMixin, View):
+@async_login_required
+@lynx_post_only
+async def add_feed_item_to_library_view(request: HttpRequest, pk):
+  feed_item = await aget_object_or_404(
+      FeedItem.objects.select_related('feed__user'),
+      pk=pk,
+      feed__user=request.user)
+  feed = feed_item.feed
 
-  def post(self, request, pk):
-    try:
-      feed = Feed.objects.get(pk=pk)
-      if feed.user != request.user:
-        raise Feed.DoesNotExist()
-      loader = feed_utils.RemoteFeedLoader(
-          request.user, request, feed=feed).load_remote_feed(
-          ).persist_new_feed_items().persist_feed()
-      if len(loader.get_new_entries()) > 0:
-        messages.success(
-            request,
-            f"Feed (ID {loader.get_feed().pk}) refreshed and {len(loader.get_new_entries())} entries added."
-        )
-      return HttpResponseRedirect(reverse("lynx:feed_items", args=(feed.pk, )))
-    except Feed.DoesNotExist:
-      return JsonResponse({"error": "Feed does not exist."})
+  url = feed_item.saved_as_link
+  if url is None:
+    url = await (sync_to_async(
+        lambda: url_parser.parse_url(feed_item.url, request.user))())
+    url.created_from_feed = feed
+    await url.asave()
+
+    feed_item.saved_as_link = url
+    await feed_item.asave()
+
+  return redirect('lynx:feed_items', feed_id=feed.pk)
 
 
-class RefreshAllFeedsView(LoginRequiredMixin, View):
+@async_login_required
+@lynx_post_only
+async def refresh_feed_from_remote_view(request: HttpRequest, pk: int) -> HttpResponse:
+  feed = await aget_object_or_404(Feed, pk=pk, user=request.user)
+  loader = await (sync_to_async(
+      lambda: feed_utils.RemoteFeedLoader(request.user, request, feed=feed).
+      load_remote_feed().persist_new_feed_items().persist_feed())())
+  if len(loader.get_new_entries()) > 0:
+    messages.success(
+        request,
+        f"Feed (ID {loader.get_feed().pk}) refreshed and {len(loader.get_new_entries())} entries added."
+    )
+  return redirect('lynx:feed_items', feed_id=feed.pk)
 
-  def post(self, request):
-    feeds = Feed.objects.filter(user=request.user).all()
-    for feed in feeds:
-      loader = feed_utils.RemoteFeedLoader(
-          request.user, request, feed=feed).load_remote_feed(
-          ).persist_new_feed_items().persist_feed()
-      if len(loader.get_new_entries()) > 0:
-        messages.success(
-            request,
-            f"Feed (ID {loader.get_feed().pk}) refreshed and {len(loader.get_new_entries())} entries added."
-        )
-    return HttpResponseRedirect(reverse("lynx:feeds", args=()))
+
+@async_login_required
+@lynx_post_only
+async def refresh_all_feeds_view(request: HttpRequest) -> HttpResponse:
+  feeds = Feed.objects.filter(user=request.user, is_deleted=False)
+  async for feed in feeds:
+    loader = await (sync_to_async(
+        lambda: feed_utils.RemoteFeedLoader(request.user, request, feed=feed).
+        load_remote_feed().persist_new_feed_items().persist_feed())())
+    if len(loader.get_new_entries()) > 0:
+      messages.success(
+          request,
+          f"Feed (ID {loader.get_feed().pk}) refreshed and {len(loader.get_new_entries())} entries added."
+      )
+  return redirect('lynx:feeds')
