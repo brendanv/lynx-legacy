a467bd19663c0f4767fe7924e3c351c39f1ac0bb
brendanv <brendanv@users.noreply.github.com>
2023-12-30 03:44:45 +0000

    Update css themes

--- lynx/jstoolchain/tailwind.config.js
+++ lynx/jstoolchain/tailwind.config.js
@@ -11,6 +11,28 @@ module.exports = {
     require('@tailwindcss/forms'),
     require("daisyui")
   ],
+  daisyui: {
+    themes: [
+      {
+        "light": {
+          ...require("daisyui/src/theming/themes")["cupcake"],
+        }
+      },
+      {
+        // Sunset, but switch two button colors, and round buttons like
+        // the light theme.
+        "dark": {
+          ...require("daisyui/src/theming/themes")["sunset"],
+          primary: require("daisyui/src/theming/themes")["sunset"]["accent"],
+          accent: require("daisyui/src/theming/themes")["sunset"]["primary"],
+          "--rounded-btn": require("daisyui/src/theming/themes")["cupcake"]["--rounded-btn"],
+          "--rounded-box": require("daisyui/src/theming/themes")["cupcake"]["--rounded-box"],
+          "--rounded-badge": require("daisyui/src/theming/themes")["cupcake"]["--rounded-badge"]
+        }
+      }
+    ],
+    darkTheme: 'dark'
+  },
   safelist: [
     {
       pattern: /alert-(info|warning|error|success|debug)/
