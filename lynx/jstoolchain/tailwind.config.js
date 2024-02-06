17930d09e3d84ca7e7d5737f08cd68094c528a18
brendanv <brendanv@users.noreply.github.com>
2024-02-06 03:54:01 +0000

    Change + simplify css themes
    I may have a problem

--- lynx/jstoolchain/tailwind.config.js
+++ lynx/jstoolchain/tailwind.config.js
@@ -12,26 +12,8 @@ module.exports = {
     require("daisyui")
   ],
   daisyui: {
-    themes: [
-      {
-        "light": {
-          ...require("daisyui/src/theming/themes")["cupcake"],
-        }
-      },
-      {
-        // Sunset, but switch two button colors, and round buttons like
-        // the light theme.
-        "dark": {
-          ...require("daisyui/src/theming/themes")["sunset"],
-          primary: require("daisyui/src/theming/themes")["sunset"]["accent"],
-          accent: require("daisyui/src/theming/themes")["sunset"]["primary"],
-          "--rounded-btn": require("daisyui/src/theming/themes")["cupcake"]["--rounded-btn"],
-          "--rounded-box": require("daisyui/src/theming/themes")["cupcake"]["--rounded-box"],
-          "--rounded-badge": require("daisyui/src/theming/themes")["cupcake"]["--rounded-badge"]
-        }
-      }
-    ],
-    darkTheme: 'dark'
+    themes: ['emerald', 'dracula'],
+    darkTheme: 'dracula'
   },
   safelist: [
     {
