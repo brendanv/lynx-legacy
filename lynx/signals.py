7840f18dc581606ef37cbad9331cd3f30c3b4d64
Brendan Viscomi <brendanv@users.noreply.github.com>
2024-03-15 13:49:51 +0000

    Move automatic archive creation to background process

--- lynx/signals.py
+++ lynx/signals.py
@@ -5,7 +5,7 @@ from httpx import ReadTimeout
 from lynx.commands import create_archive_for_link
 
 from lynx.models import FeedItem, Link, UserSetting
-from lynx.tasks import add_feed_item_to_library, summarize_link_in_background
+from lynx.tasks import add_feed_item_to_library, create_archive_for_link_in_background, summarize_link_in_background
 from lynx.utils.singlefile import is_singlefile_enabled
 
 
@@ -21,8 +21,9 @@ def save_feed_item_to_library(sender, instance: FeedItem, created, **kwargs):
     return
   add_feed_item_to_library(feed.user.pk, instance.pk)
 
-# When a Link is saved, if the user's settings indicate 
-# that the Link should automatically be summarized, 
+
+# When a Link is saved, if the user's settings indicate
+# that the Link should automatically be summarized,
 # then spawn a background task to summarize the Link.
 @receiver(post_save, sender=Link, dispatch_uid='summarize_link')
 def summarize_link(sender, instance: Link, created, **kwargs):
@@ -33,7 +34,8 @@ def summarize_link(sender, instance: Link, created, **kwargs):
     return
   summarize_link_in_background(instance.user.pk, instance.pk)
 
-# When a new Link is saved, create an archive of the link content 
+
+# When a new Link is saved, create an archive of the link content
 # if we have archive creation enabled in the server's environment.
 @receiver(post_save, sender=Link, dispatch_uid='create_archive_for_new_links')
 def create_archive_for_new_links(sender, instance: Link, created, **kwargs):
@@ -41,7 +43,4 @@ def create_archive_for_new_links(sender, instance: Link, created, **kwargs):
     return
   if not is_singlefile_enabled():
     return
-  try:
-    async_to_sync(create_archive_for_link)(instance.user, instance)
-  except ReadTimeout:
-    pass
+  create_archive_for_link_in_background(instance.user.pk, instance.pk)
