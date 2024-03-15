36710a5349d535140ea6f00715ac25fb8932e529
Brendan Viscomi <brendanv@users.noreply.github.com>
2024-03-15 02:26:34 +0000

    Automatically create archives of new links

--- lynx/signals.py
+++ lynx/signals.py
@@ -1,8 +1,12 @@
+from asgiref.sync import async_to_sync
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from httpx import ReadTimeout
+from lynx.commands import create_archive_for_link
 
 from lynx.models import FeedItem, Link, UserSetting
 from lynx.tasks import add_feed_item_to_library, summarize_link_in_background
+from lynx.utils.singlefile import is_singlefile_enabled
 
 
 # When a feed item is created, if the linked Feed has
@@ -28,3 +32,16 @@ def summarize_link(sender, instance: Link, created, **kwargs):
   if not setting.automatically_summarize_new_links:
     return
   summarize_link_in_background(instance.user.pk, instance.pk)
+
+# When a new Link is saved, create an archive of the link content 
+# if we have archive creation enabled in the server's environment.
+@receiver(post_save, sender=Link, dispatch_uid='create_archive_for_new_links')
+def create_archive_for_new_links(sender, instance: Link, created, **kwargs):
+  if not created:
+    return
+  if not is_singlefile_enabled():
+    return
+  try:
+    async_to_sync(create_archive_for_link)(instance.user, instance)
+  except ReadTimeout:
+    pass
